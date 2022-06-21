5aa21452108ab053072116d7d3c1e428f59554f4
Max Nadvornyi <ipzk201_nmyu@student.ztu.edu.ua>
2022-06-21 17:04:35 +0300

    todolist

---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+import {Provider} from 'react-redux';
+
+import '../node_modules/bulma/css/bulma.min.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+
+import App from './App';
+import store from './store/store';
+ 
+
+/*const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Provider store = {store}>
+      <App />
+    </Provider>
+    
+  </React.StrictMode>
+);*/
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>     
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
